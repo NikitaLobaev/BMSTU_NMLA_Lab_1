934a48cd6de2e3d0cc49fd040f4a7a367e1ef232
Nikita Lobaev <lobaevni@yandex-team.ru>
2021-10-06 20:48:02 +0300

    Optimize basic vector and matrix tests

--- tests.cpp
+++ tests.cpp
@@ -5,12 +5,12 @@ using namespace Lobaev::Math;
 
 TEST(vector, size_ok) {
     const size_t size = 3;
-    Vector<int> vector(size);
+    const Vector<int> vector(size);
 
     ASSERT_EQ(vector.size(), size);
 }
 
-TEST(vector, empty) {
+TEST(vector, size_empty_fail) {
     ASSERT_ANY_THROW({
         Vector<int>(0);
     });
@@ -59,13 +59,15 @@ TEST(vector, operator_at_const_fail) {
 }
 
 TEST(vector, operator_equals_1) {
-    Vector<int> vector1(2);
-    vector1(0) = 1;
-    vector1(1) = 13;
+    Vector<int> vector1({
+        1,
+        13
+    });
 
-    Vector<int> vector2(2);
-    vector2(0) = 1;
-    vector2(1) = 13;
+    Vector<int> vector2({
+        1,
+        13
+    });
 
     ASSERT_EQ(vector1, vector2);
 
@@ -75,26 +77,27 @@ TEST(vector, operator_equals_1) {
 }
 
 TEST(vector, operator_equals_2) {
-    Vector<int> vector1(2);
-    Vector<int> vector2(3);
+    const Vector<int> vector1(2);
+    const Vector<int> vector2(3);
 
     ASSERT_NE(vector1, vector2);
 }
 
 TEST(vector, multiply_ok) {
-    Vector<int> vector1(5);
-    vector1(0) = 1;
-    vector1(1) = 2;
-    vector1(2) = 3;
-    vector1(3) = 4;
-    vector1(4) = 5;
-
-    Vector<int> vector2(5);
-    vector2(0) = 5;
-    vector2(1) = 4;
-    vector2(2) = 3;
-    vector2(3) = 2;
-    vector2(4) = 1;
+    const Vector<int> vector1({
+        1,
+        2,
+        3,
+        4,
+        5
+    });
+    const Vector<int> vector2({
+        5,
+        4,
+        3,
+        2,
+        1
+    });
 
     const int result = vector1 * vector2;
 
@@ -106,7 +109,7 @@ TEST(vector, multiply_ok) {
 }
 
 TEST(vector, multiply_fail) {
-    Vector<int> vector1(2), vector2(3);
+    const Vector<int> vector1(2), vector2(3);
 
     ASSERT_ANY_THROW({
         vector1 * vector2;
@@ -141,7 +144,6 @@ TEST(vector, plus_ok) {
         0,
         -11
     });
-
     const Vector<int> vector2({
         5,
         3,
@@ -168,7 +170,6 @@ TEST(vector, plus_dimensions_fail) {
         1,
         2
     });
-
     const Vector<int> vector2({
         3,
         4,
@@ -188,7 +189,6 @@ TEST(vector, minus_ok) {
         9,
         0
     });
-
     const Vector<int> vector2({
         -1,
         3,
@@ -216,7 +216,6 @@ TEST(vector, minus_dimensions_fail) {
         4,
         5
     });
-
     const Vector<int> vector2({
         1,
         2
@@ -322,7 +321,7 @@ TEST(vector, norm_infinite_ok) {
 
 TEST(matrix, dimensions_ok) {
     const size_t m = 2, n = 3;
-    Matrix<int> matrix(m, n);
+    const Matrix<int> matrix(m, n);
 
     ASSERT_EQ(matrix.rows_count(), m);
     ASSERT_EQ(matrix.columns_count(), n);
@@ -363,17 +362,14 @@ TEST(matrix, operator_at_failed) {
 }
 
 TEST(matrix, operator_equals_1) {
-    Matrix<int> matrix1(2, 2);
-    matrix1(0, 0) = 1;
-    matrix1(0, 1) = 2;
-    matrix1(1, 0) = 3;
-    matrix1(1, 1) = 4;
-
-    Matrix<int> matrix2(2, 2);
-    matrix2(0, 0) = 1;
-    matrix2(0, 1) = 2;
-    matrix2(1, 0) = 3;
-    matrix2(1, 1) = 4;
+    Matrix<int> matrix1({
+        {1, 2},
+        {3, 4}
+    });
+    Matrix<int> matrix2({
+        {1, 2},
+        {3, 4}
+    });
 
     ASSERT_EQ(matrix1, matrix2);
 
@@ -383,37 +379,35 @@ TEST(matrix, operator_equals_1) {
 }
 
 TEST(matrix, operator_equals_2) {
-    Matrix<int> matrix1(2, 3), matrix2(3, 2);
+    const Matrix<int> matrix1(2, 3), matrix2(3, 2);
 
     ASSERT_NE(matrix1, matrix2);
 }
 
 TEST(matrix, operator_multiply_vector_ok) {
-    Matrix<int> matrix(2, 3);
-    matrix(0, 0) = 1;
-    matrix(0, 1) = 2;
-    matrix(0, 2) = 3;
-    matrix(1, 0) = 4;
-    matrix(1, 1) = 5;
-    matrix(1, 2) = 6;
+    const Matrix<int> matrix({
+        {1, 2, 3},
+        {4, 5, 6}
+    });
+    const Vector<int> vector({
+        7,
+        8,
+        9
+    });
 
-    Vector<int> vector(3);
-    vector(0) = 7;
-    vector(1) = 8;
-    vector(2) = 9;
+    const Vector<int> result = matrix * vector;
 
-    Vector<int> result = matrix * vector;
-
-    Vector<int> expected_result(2);
-    expected_result(0) = 50;
-    expected_result(1) = 122;
+    const Vector<int> expected_result({
+        50,
+        122
+    });
 
     ASSERT_EQ(result, expected_result);
 }
 
 TEST(matrix, operator_multiply_vector_fail) {
-    Matrix<int> matrix(2, 3);
-    Vector<int> vector(2);
+    const Matrix<int> matrix(2, 3);
+    const Vector<int> vector(2);
 
     ASSERT_ANY_THROW({
         matrix * vector;
@@ -421,35 +415,28 @@ TEST(matrix, operator_multiply_vector_fail) {
 }
 
 TEST(matrix, operator_multiply_matrix_ok) {
-    Matrix<int> matrix1(2, 3);
-    matrix1(0, 0) = 1;
-    matrix1(0, 1) = 2;
-    matrix1(0, 2) = 3;
-    matrix1(1, 0) = 4;
-    matrix1(1, 1) = 5;
-    matrix1(1, 2) = 6;
+    const Matrix<int> matrix1({
+        {1, 2, 3},
+        {4, 5, 6}
+    });
+    const Matrix<int> matrix2({
+        {7, 8},
+        {9, 10},
+        {11, 12}
+    });
 
-    Matrix<int> matrix2(3, 2);
-    matrix2(0, 0) = 7;
-    matrix2(0, 1) = 8;
-    matrix2(1, 0) = 9;
-    matrix2(1, 1) = 10;
-    matrix2(2, 0) = 11;
-    matrix2(2, 1) = 12;
+    const Matrix<int> result = matrix1 * matrix2;
 
-    Matrix<int> result = matrix1 * matrix2;
-
-    Matrix<int> expected_result(2, 2);
-    expected_result(0, 0) = 58;
-    expected_result(0, 1) = 64;
-    expected_result(1, 0) = 139;
-    expected_result(1, 1) = 154;
+    const Matrix<int> expected_result({
+        {58, 64},
+        {139, 154}
+    });
 
     ASSERT_EQ(result, expected_result);
 }
 
 TEST(matrix, operator_multiply_matrix_fail) {
-    Matrix<int> matrix1(2, 3), matrix2(2, 3);
+    const Matrix<int> matrix1(2, 3), matrix2(2, 3);
 
     ASSERT_ANY_THROW({
         matrix1 * matrix2;
@@ -457,33 +444,28 @@ TEST(matrix, operator_multiply_matrix_fail) {
 }
 
 TEST(matrix, is_square_1_ok) {
-    Matrix<int> matrix(2, 2);
+    const Matrix<int> matrix(2, 2);
     ASSERT_TRUE(matrix.is_square());
 }
 
 TEST(matrix, is_square_2_ok) {
-    Matrix<int> matrix(2, 3);
+    const Matrix<int> matrix(2, 3);
     ASSERT_FALSE(matrix.is_square());
 }
 
 TEST(matrix, transpose_ok) {
-    Matrix<int> matrix(2, 3);
-    matrix(0, 0) = 1;
-    matrix(0, 1) = 2;
-    matrix(0, 2) = 3;
-    matrix(1, 0) = 4;
-    matrix(1, 1) = 5;
-    matrix(1, 2) = 6;
+    const Matrix<int> matrix({
+        {1, 2, 3},
+        {4, 5, 6}
+    });
 
-    Matrix<int> result_matrix = matrix.transpose();
+    const Matrix<int> result_matrix = matrix.transpose();
 
-    Matrix<int> expected_matrix(3, 2);
-    expected_matrix(0, 0) = 1;
-    expected_matrix(0, 1) = 4;
-    expected_matrix(1, 0) = 2;
-    expected_matrix(1, 1) = 5;
-    expected_matrix(2, 0) = 3;
-    expected_matrix(2, 1) = 6;
+    const Matrix<int> expected_matrix({
+        {1, 4},
+        {2, 5},
+        {3, 6}
+    });
 
     ASSERT_EQ(result_matrix, expected_matrix);
 }
